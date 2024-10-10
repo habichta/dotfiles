f549b22391d584fc788729ed5446cd41b6925560
Arthur Habicht <arthur.habicht@helpany.com>
2024-10-10 07:55:34 +0200

    Improved AG and git af

--- configs/.config/nvim/lua/functions/neovim.lua
+++ configs/.config/nvim/lua/functions/neovim.lua
@@ -3,24 +3,33 @@ vim.api.nvim_set_keymap('n', ',gv', ':vertical wincmd f<CR>', { noremap = true, 
 vim.api.nvim_set_keymap('n', ',gh', ':wincmd f<CR>', { noremap = true, silent = true })
 
 function VisualAg()
-  -- Get the selected text in visual mode
-  local _, start_line, start_col, _ = unpack(vim.fn.getpos("'<"))
-  local _, end_line, end_col, _ = unpack(vim.fn.getpos("'>"))
+  -- Check if we are in visual mode or normal mode
+  local mode = vim.fn.mode()
 
-  -- Get the lines in the visual selection
-  local lines = vim.fn.getline(start_line, end_line)
+  local search_term = ""
 
-  -- Handle single line selection
-  if #lines == 1 then
-    lines[1] = string.sub(lines[1], start_col, end_col - 1)   -- Adjust for correct indexing
+  if mode == "v" then
+    -- In visual mode: Get the selected text in visual mode
+    local _, start_line, start_col, _ = unpack(vim.fn.getpos("'<"))
+    local _, end_line, end_col, _ = unpack(vim.fn.getpos("'>"))
+
+    -- Get the lines in the visual selection
+    local lines = vim.fn.getline(start_line, end_line)
+
+    -- Handle single line selection
+    if #lines == 1 then
+      lines[1] = string.sub(lines[1], start_col, end_col)       -- Adjust for correct indexing
+    else
+      lines[1] = string.sub(lines[1], start_col)                -- Start from start_col
+      lines[#lines] = string.sub(lines[#lines], 1, end_col - 1) -- End at end_col - 1
+    end
+
+    -- Join the lines into a single search string
+    search_term = table.concat(lines, " ")
   else
-    lines[1] = string.sub(lines[1], start_col)                -- Start from start_col
-    lines[#lines] = string.sub(lines[#lines], 1, end_col - 1) -- End at end_col - 1
+    -- In normal mode: Get the word under the cursor
+    search_term = vim.fn.expand("<cword>")
   end
-
-
-  -- Join the lines into a single search string
-  local search_term = table.concat(lines, " ")
 
   -- Escape special characters for searching
   search_term = vim.fn.escape(search_term, '\\*[](){}?+|^$')
@@ -35,5 +44,20 @@ function VisualAg()
   vim.cmd('Ag ' .. search_term)
 end
 
--- Map the key combination for visual mode
-vim.api.nvim_set_keymap('v', '<leader>ag', ':lua VisualAg()<CR>', { noremap = true, silent = true })
+-- Map the key combination for normal and visual mode
+vim.api.nvim_set_keymap('n', '<leader>pp', ':lua VisualAg()<CR>', { noremap = true, silent = true }) --
+vim.api.nvim_set_keymap('v', '<leader>pp', ':lua VisualAg()<CR>', { noremap = true, silent = true }) --
+
+
+-- Function to move the rest of the line up
+function MoveLineUp()
+  local current_pos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd("normal! d$")
+  vim.cmd("normal! k")
+  vim.cmd("normal! o")
+  vim.cmd("normal! P")
+  vim.api.nvim_win_set_cursor(0, { current_pos[1] - 1, 0 })
+end
+
+-- Create the mapping
+vim.api.nvim_set_keymap('n', '<leader>J', ':lua MoveLineUp()<CR>', { noremap = true, silent = true })
